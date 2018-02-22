3331bf02f13eafb74ae546ee650aa78a642d82f4
unknown <yanjuanyan@360buyAD.local>
2018-02-22 17:29:41 +0800

    123

---
+++ src/config.js
@@ -0,0 +1,44 @@
+const env = 'production' // 'development' or 'production'
+
+const version = 2.0
+
+const hosts = {
+  development: 'http://localhost:3020',
+  production: 'https://www.163.com'
+}
+
+const api = {
+  user: {
+    /**
+     * login api
+     * need header:
+     * {
+     *   'x-wechat-code': code,
+     *   'x-wechat-encrypted': encryptedData,
+     *   'x-wechat-iv': iv
+     * }
+     */
+    login: {
+      method: 'POST',
+      url: '/api/wx/login'
+    }
+  }
+}
+
+module.exports = {
+  env,
+  version,
+  api: disposeUrl(api, hosts[env])
+}
+
+function disposeUrl (obj, prefix) {
+  Object.keys(obj).forEach(v => {
+    if (obj[v].url) {
+      obj[v].url = prefix + obj[v].url
+    } else {
+      obj[v] = disposeUrl(obj[v], prefix)
+    }
+  })
+
+  return obj
+}
